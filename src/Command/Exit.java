2c53941b158eb8ba55f71b606ce599973a0726d2
Shane <WST773@student.bham.ac.uk>
2018-02-04 22:30:43 +0000

    Ask do you want to exit

--- src/Command/Exit.java
+++ src/Command/Exit.java
@@ -1,5 +1,6 @@
 package Command;
 
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
 public class Exit extends Command {
@@ -7,7 +8,11 @@ public class Exit extends Command {
 	@Override
 	public JPanel execute() {
 		// TODO Auto-generated method stub
-		System.exit(0);
+		
+		int i = JOptionPane.showConfirmDialog(null, "Are you sure you want to exit the game?", "Confirm exit", JOptionPane.YES_NO_OPTION);
+		if (i == JOptionPane.YES_OPTION) {
+			System.exit(0);
+		}
 		return null;
 	}
 
